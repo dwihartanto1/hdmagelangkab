4f240e5e4c751ac33bc4dc953c338d5ecac14ea0
Dwi Hartanto <dwihartanto1991@gmail.com>
2025-12-23 12:48:28 +0700

    Update config comment

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from "astro/config";
 
 import vercel from "@astrojs/vercel";
 
+// Configuration for HD Magelang Directory
 // https://astro.build/config
 export default defineConfig({
   // Aktifkan mode server agar konten otomatis update di Vercel
